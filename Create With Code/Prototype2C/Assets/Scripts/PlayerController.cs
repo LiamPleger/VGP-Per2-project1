0683fec7324ee518d188db43522bd77e1404e04d
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2022-10-10 09:20:20 -0700

    Projectiles and second border added

--- Create With Code/Prototype2C/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype2C/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 10.0f;
+    public GameObject projectilePrefab;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,8 +17,11 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -10) {
-            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange) {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange) {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
