8a261371f87d921579a0c297b2630a2168cf48a2
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2022-11-01 09:07:32 -0700

    Delays, Lots of Delays

--- Create With Code/Prototype2C/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Create With Code/Prototype2C/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,19 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-
+    private float waitTime = 1;
+    private float newTime = 0;
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > newTime)
         {
+
+            newTime = Time.time + waitTime;
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+
+        
         }
     }
 }
