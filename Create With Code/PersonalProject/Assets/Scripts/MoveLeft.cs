b24691f88c0bcdc6cd5c7215cf03a3f0ea191a9f
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2023-02-02 09:14:49 -0800

    MoveLeft script finished

---
+++ Create With Code/PersonalProject/Assets/Scripts/MoveLeft.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveLeft : MonoBehaviour
+{
+    private float speed = 5.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.gameObject.CompareTag("Belt"))
+        {
+            transform.Translate(Vector3.right * Time.deltaTime * speed);
+        } 
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+       
+        if (other.gameObject.CompareTag("Flame"))
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+}
