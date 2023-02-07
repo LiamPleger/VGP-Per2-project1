a4605098f15f03a1c84184b6f92b3b1016a3f69f
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2023-02-07 09:13:31 -0800

    Spawn Manager

--- Create With Code/PersonalProject/Assets/Scripts/SpawnItems.cs
+++ Create With Code/PersonalProject/Assets/Scripts/SpawnItems.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class SpawnItems : MonoBehaviour
 {
+    public GameObject[] itemPrefabs;
+    private float spawnRangeX = -18.0f;
+    private float spawnPosZ = 0.0f;
+    private float height = .9f;
+    private float startDelay = 2.0f;
+    private float spawnInterval = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomObject", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    void SpawnRandomObject() {
+        //Spawn animals
+        int itemIndex = Random.Range(0, itemPrefabs.Length);
+        Vector3 spawnPos = new Vector3(spawnRangeX, height, spawnPosZ);
+
+        Instantiate(itemPrefabs[itemIndex], spawnPos, itemPrefabs[itemIndex].transform.rotation);
     }
 }
