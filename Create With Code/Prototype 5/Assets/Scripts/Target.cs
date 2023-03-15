86f4f0c4dbd60c1d5eaf8869f80aa4c0cb6c26e7
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2023-03-15 09:11:16 -0700

    Something done probably

--- Create With Code/Prototype 5/Assets/Scripts/Target.cs
+++ Create With Code/Prototype 5/Assets/Scripts/Target.cs
@@ -12,6 +12,7 @@ public class Target : MonoBehaviour
     private float maxTorque = 10;
     private float xRange = 4;
     private float ySpawnPos = -4;
+    public ParticleSystem explosionParticle;
 
     // Start is called before the first frame update
     void Start()
@@ -33,6 +34,7 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown() {
         Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
         gameManager.UpdateScore(pointValue);
     }
 
