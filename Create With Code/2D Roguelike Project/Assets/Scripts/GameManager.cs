7ac770650ac6d254ae00c95681de48a2abc7c1ab
LiamPleger <74025906+LiamPleger@users.noreply.github.com>
2023-05-12 09:03:40 -0700

    Scripts

--- Create With Code/2D Roguelike Project/Assets/Scripts/GameManager.cs
+++ Create With Code/2D Roguelike Project/Assets/Scripts/GameManager.cs
@@ -4,22 +4,51 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public BoardManager boardScript;
-
+    //Static instance of GameManager which allows it to be accessed by any other script.
+    public static GameManager instance = null;
+    //Store a reference to our BoardManager which will set up the level.
+    private BoardManager boardScript;
+    //Current level number, expressed in game as "Day  1".
     private int level = 3;
-    // Start is called before the first frame update
+
+    //Awake is always called before any Start functions
     void Awake()
     {
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);    
+
+        //Sets this to not be destroyed when reloading scene
+        DontDestroyOnLoad(gameObject);
+
+        //Get a component reference to the attached BoardManager script
         boardScript = GetComponent<BoardManager>();
+
+        //Call the InitGame function to initialize the first level 
         InitGame();
     }
-    void InitGame() {
+
+    //Initializes the game for each level.
+    void InitGame()
+    {
+        //Call the SetupScene function of the BoardManager script, pass it current level number.
         boardScript.SetupScene(level);
+
     }
 
-    // Update is called once per frame
+
+
+    //Update is called every frame.
     void Update()
     {
-        
+
     }
 }
